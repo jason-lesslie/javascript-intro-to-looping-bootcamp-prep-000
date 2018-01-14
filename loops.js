190ea6267ee446a5af29cee490915f24c1a94705
Jason Lesslie <jason.lesslie@gmail.com>
2018-01-14 18:12:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,3 +18,10 @@ function whileLoop(n) {
 function maybeTrue() {
   return Math.random() >= 0.5
 }
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  }
+  while (array.length > 0 && maybeTrue());
+  return array
+}
